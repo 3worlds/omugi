bfdb614282ac680473f476b35bc32a221f642daa
Ian Davies <u9007333@anu.edu.au>
2019-02-15 14:41:31 +1100

    delete test crap

--- omugi/src/fr/cnrs/iees/playground/graphs/AotGraph2.java
+++ omugi/src/fr/cnrs/iees/playground/graphs/AotGraph2.java
@@ -30,16 +30,11 @@
  **************************************************************************/
 package fr.cnrs.iees.playground.graphs;
 
-import java.io.File;
-
 import fr.cnrs.iees.playground.elements.IEdge;
 import fr.cnrs.iees.playground.elements.INode;
 import fr.cnrs.iees.playground.elements.ITreeNode;
 import fr.cnrs.iees.playground.factories.IEdgeFactory;
 import fr.cnrs.iees.playground.factories.ITreeNodeFactory;
-import fr.cnrs.iees.playground.io.OmugiGraphImporter2;
-import fr.cnrs.iees.playground.io.TreeGraphParser2;
-import fr.cnrs.iees.playground.io.TreeGraphTokenizer2;
 import fr.cnrs.iees.properties.impl.ExtendablePropertyListImpl;
 
 public class AotGraph2 extends TreeGraph2<AotNode2, AotEdge2> implements ITreeNodeFactory, IEdgeFactory// ,
@@ -48,22 +43,6 @@ public class AotGraph2 extends TreeGraph2<AotNode2, AotEdge2> implements ITreeNo
 
 	public AotGraph2() {
 		super();
-	}
-	
-	// OR
-	public AotGraph2(File file) {
-		this();
-		// lines 
-		// TreeGraphTokenizer
-		// TreeGraphReader/Parser
-	}
-//OR
-	// Use a reader pattern???
-	protected AotGraph2(TreeGraphTokenizer2 tokenizer) {
-		this();
-		TreeGraphParser2 tgp = new TreeGraphParser2(tokenizer,this,this);
-//		this.nodes = tgp.getNodes();
-//		this.root = tgp.getRoot();
 	}
 
 	@Override
@@ -80,11 +59,5 @@ public class AotGraph2 extends TreeGraph2<AotNode2, AotEdge2> implements ITreeNo
 		return node;
 	}
 
-	// maybe 
-	public static AotGraph2 importGraph(File file){
-		AotGraph2 factories = new AotGraph2();
-		OmugiGraphImporter2 gi = new OmugiGraphImporter2(file, factories, null, factories);
-		return (AotGraph2) gi.getGraph();		
-	}
 
 }
