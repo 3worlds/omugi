1953ef76932fd5ef2211fa26985514a3b5ccf5f5
Ian Davies <u9007333@anu.edu.au>
2019-03-22 17:48:42 +1100

    issue $14 import parser

--- omugi/src/fr/cnrs/iees/io/parsing/impl/TreeParser.java
+++ omugi/src/fr/cnrs/iees/io/parsing/impl/TreeParser.java
@@ -46,26 +46,42 @@ import fr.cnrs.iees.io.parsing.impl.TreeTokenizer.treeToken;
 import fr.cnrs.iees.properties.PropertyListFactory;
 
 /**
- * <p>A parser for tree data.</p>
+ * <p>
+ * A parser for tree data.
+ * </p>
  * 
- * <p>This parser is initialised with a {@link TreeTokenizer}, i.e. it gobbles a list of
- * tokens and spits out a {@link Tree} when asked for it. It is lazy, i.e. it will not do anything until
- * asked for a graph, i.e. invoking the {@code TreeParser.graph()} method, and it will parse only once after
- * initialisation. Further calls to {@code .graph()} return the already parsed tree.</p>
+ * <p>
+ * This parser is initialised with a {@link TreeTokenizer}, i.e. it gobbles a
+ * list of tokens and spits out a {@link Tree} when asked for it. It is lazy,
+ * i.e. it will not do anything until asked for a graph, i.e. invoking the
+ * {@code TreeParser.graph()} method, and it will parse only once after
+ * initialisation. Further calls to {@code .graph()} return the already parsed
+ * tree.
+ * </p>
  *
- * <p>Parsing is done in a single pass on the token list.</p>
+ * <p>
+ * Parsing is done in a single pass on the token list.
+ * </p>
  * 
- * <p>Options to setup the tree may be passed through tree-level properties in the file. These 
- * are found in {@link TreeProperties}. The best way to go is to implement specific {@link TreeNodeFactory}
- * and {@link PropertyListFactory}
- * which will implement which flavour of {@link TreeNode} and property list
- * (cf. {@link PropertyListGetters} descendants) should be used to construct
- * the tree.</p> 
+ * <p>
+ * Options to setup the tree may be passed through tree-level properties in the
+ * file. These are found in {@link TreeProperties}. The best way to go is to
+ * implement specific {@link TreeNodeFactory} and {@link PropertyListFactory}
+ * which will implement which flavour of {@link TreeNode} and property list (cf.
+ * {@link PropertyListGetters} descendants) should be used to construct the
+ * tree.
+ * </p>
  * 
- * <p>Allowed property types are those listed in {@link ValidPropertyTypes}. They can be
- * specified as fully qualified java class names, or as simple strings as found in {@code ValidPropertyTypes}.</p>
+ * <p>
+ * Allowed property types are those listed in {@link ValidPropertyTypes}. They
+ * can be specified as fully qualified java class names, or as simple strings as
+ * found in {@code ValidPropertyTypes}.
+ * </p>
  * 
- *  <p>Note that the best use of this class is to hide it inside a {@link GraphImporter}.</p>
+ * <p>
+ * Note that the best use of this class is to hide it inside a
+ * {@link GraphImporter}.
+ * </p>
  * 
  * @author Jacques Gignoux - 20 déc. 2018
  *
@@ -74,23 +90,22 @@ public class TreeParser extends MinimalGraphParser {
 
 	private Logger log = Logger.getLogger(TreeParser.class.getName());
 
-	//----------------------------------------------------
+	// ----------------------------------------------------
 	// which type of item is currently being constructed
 	private enum itemType {
-		TREE,
-		NODE,
+		TREE, NODE,
 	}
 
 	// the tokenizer used to read the file
 	private TreeTokenizer tokenizer;
-	
+
 	// the result of this parsing
 	private Tree<? extends TreeNode> tree = null;
 
 	// the list of specifications built from the token list
 	private List<propSpec> treeProps = new LinkedList<propSpec>();
-	private List<treeNodeSpec> nodeSpecs =  new LinkedList<treeNodeSpec>();
-	
+	private List<treeNodeSpec> nodeSpecs = new LinkedList<treeNodeSpec>();
+
 	// the last processed item
 	private itemType lastItem = null;
 	private treeNodeSpec[] lastNodes = null;
@@ -98,59 +113,62 @@ public class TreeParser extends MinimalGraphParser {
 
 	// factories for treenodes and properties
 	private TreeNodeFactory treeFactory = null;
-	
 
 	public TreeParser(TreeTokenizer tokenizer) {
 		super();
-		this.tokenizer =tokenizer;
+		this.tokenizer = tokenizer;
 	}
 
 	@Override
 	protected void parse() {
 		if (!tokenizer.tokenized())
 			tokenizer.tokenize();
-		lastNodes = new treeNodeSpec[tokenizer.maxDepth()+1];
+		lastNodes = new treeNodeSpec[tokenizer.maxDepth() + 1];
 		lastItem = itemType.TREE;
 		while (tokenizer.hasNext()) {
 			treeToken tk = tokenizer.getNextToken();
 			switch (tk.type) {
-				case COMMENT:
-					break;
-				case LABEL:
-					int level = tk.level;
-					lastNodes[level] = new treeNodeSpec();
-					lastNodes[level].label = tk.value;
-					if (level>0)
-						lastNodes[level].parent = lastNodes[level-1];
-					lastItem = itemType.NODE;
-					break;
-				case LEVEL:
-					// such tokens should never be created
-					break;
-				case NAME:
-					level = tk.level;
-					lastNodes[level].name = tk.value;
-					nodeSpecs.add(lastNodes[level]);
-					break;
-				case IMPORT_RESOURCE:
-					System.out.println(tk.value);
-					// -1 if no graph props! -0 otherwise
-					System.out.println(lastNodes[tk.level-1].label+":"+lastNodes[tk.level-1].name);
-					break;
-				case IMPORT_FILE:
-					// stuffed here also.
-				    break;
-				case PROPERTY_NAME:
-					lastProp = new propSpec();
-					lastProp.name = tk.value;
-					break;
-				case PROPERTY_TYPE:
-					lastProp.type = tk.value;
-					break;
-				case PROPERTY_VALUE:
-					lastProp.value = tk.value;
-					if (lastItem==itemType.TREE)
-						treeProps.add(lastProp);
+			case COMMENT:
+				break;
+			case LABEL:
+				int level = tk.level;
+				lastNodes[level] = new treeNodeSpec();
+				lastNodes[level].label = tk.value;
+				if (level > 0)
+					lastNodes[level].parent = lastNodes[level - 1];
+				lastItem = itemType.NODE;
+				break;
+			case LEVEL:
+				// such tokens should never be created
+				break;
+			case NAME:
+				level = tk.level;
+				lastNodes[level].name = tk.value;
+				nodeSpecs.add(lastNodes[level]);
+				break;
+			case IMPORT_RESOURCE:
+				// bit stuffed now: we have only specs, not graph or parent node.
+				System.out.println(tk.value);
+				System.out.println(lastNodes[tk.level - 1].label + ":" + lastNodes[tk.level - 1].name);
+				/*
+				 * we need a stub to load the resource later. Messy. Better if all this is
+				 * simple recursion
+				 */
+				break;
+			case IMPORT_FILE:
+				// stuffed here also.
+				break;
+			case PROPERTY_NAME:
+				lastProp = new propSpec();
+				lastProp.name = tk.value;
+				break;
+			case PROPERTY_TYPE:
+				lastProp.type = tk.value;
+				break;
+			case PROPERTY_VALUE:
+				lastProp.value = tk.value;
+				if (lastItem == itemType.TREE)
+					treeProps.add(lastProp);
 				// i.e if not a graph property
 				else if (lastNodes[tk.level - 1] != null)
 					lastNodes[tk.level - 1].props.add(lastProp);
@@ -162,21 +180,20 @@ public class TreeParser extends MinimalGraphParser {
 			}
 		}
 	}
-	
+
 	// builds the tree from the parsed data
 	@SuppressWarnings("unchecked")
 	private void buildTree() {
-		if (lastNodes==null)
+		if (lastNodes == null)
 			parse();
 		Class<? extends Tree<? extends TreeNode>> treeClass = null;
 		Class<? extends TreeNodeFactory> tFactoryClass = null;
 		Class<? extends PropertyListFactory> plFactoryClass = null;
 		// scan tree properties for tree building options
-		for (propSpec p:treeProps) {
+		for (propSpec p : treeProps) {
 			switch (TreeProperties.propertyForName(p.name)) {
 			case CLASS:
-				treeClass = (Class<? extends Tree<? extends TreeNode>>) 
-					getClass(TreeProperties.CLASS,p.value,log);
+				treeClass = (Class<? extends Tree<? extends TreeNode>>) getClass(TreeProperties.CLASS, p.value, log);
 				break;
 			case MUTABLE:
 // TODO: implement MutableTreeImpl				
@@ -184,27 +201,23 @@ public class TreeParser extends MinimalGraphParser {
 //					getClass(TreeProperties.CLASS,MutableTreeImpl.class.getName());
 				break;
 			case PROP_FACTORY:
-				plFactoryClass = (Class<? extends PropertyListFactory>) 
-					getClass(TreeProperties.PROP_FACTORY,p.value,log);
+				plFactoryClass = (Class<? extends PropertyListFactory>) getClass(TreeProperties.PROP_FACTORY, p.value,
+						log);
 				break;
 			case TREE_FACTORY:
-				tFactoryClass = (Class<? extends TreeNodeFactory>) 
-					getClass(TreeProperties.TREE_FACTORY,p.value,log);
+				tFactoryClass = (Class<? extends TreeNodeFactory>) getClass(TreeProperties.TREE_FACTORY, p.value, log);
 				break;
 			default:
 				break;
 			}
 		}
 		// use default settings if graph properties were absent
-		if (treeClass==null)
-			treeClass = (Class<? extends Tree<? extends TreeNode>>) 
-				getClass(TreeProperties.CLASS,log);
-		if (tFactoryClass==null)
-			tFactoryClass = (Class<? extends TreeNodeFactory>) 
-				getClass(TreeProperties.TREE_FACTORY,log);
-		if (plFactoryClass==null)
-			plFactoryClass = (Class<? extends PropertyListFactory>) 
-				getClass(TreeProperties.PROP_FACTORY,log);
+		if (treeClass == null)
+			treeClass = (Class<? extends Tree<? extends TreeNode>>) getClass(TreeProperties.CLASS, log);
+		if (tFactoryClass == null)
+			tFactoryClass = (Class<? extends TreeNodeFactory>) getClass(TreeProperties.TREE_FACTORY, log);
+		if (plFactoryClass == null)
+			plFactoryClass = (Class<? extends PropertyListFactory>) getClass(TreeProperties.PROP_FACTORY, log);
 		// setup the factories
 		try {
 			treeFactory = tFactoryClass.newInstance();
@@ -215,27 +228,26 @@ public class TreeParser extends MinimalGraphParser {
 			e.printStackTrace();
 		}
 		// make tree nodes
-		Map<String,TreeNode> nodes = new HashMap<>();
-		for (treeNodeSpec ns:nodeSpecs) {
+		Map<String, TreeNode> nodes = new HashMap<>();
+		for (treeNodeSpec ns : nodeSpecs) {
 			TreeNode n = null;
 			Class<? extends TreeNode> nc = treeFactory.treeNodeClass(ns.label);
 			if (ns.props.isEmpty())
-				if (nc==null)
-					n = treeFactory.makeTreeNode(null,ns.name);
+				if (nc == null)
+					n = treeFactory.makeTreeNode(null, ns.name);
 				else
-					n = treeFactory.makeTreeNode(nc,null,ns.name);
+					n = treeFactory.makeTreeNode(nc, null, ns.name);
+			else if (nc == null)
+				n = treeFactory.makeTreeNode(null, ns.name, makePropertyList(ns.props, log));
 			else
-				if (nc==null)
-					n = treeFactory.makeTreeNode(null,ns.name,makePropertyList(ns.props,log));
-				else
-					n = treeFactory.makeTreeNode(nc,null,ns.name,makePropertyList(ns.props,log));
-			String nodeId = ns.label.trim()+":"+ns.name.trim();
+				n = treeFactory.makeTreeNode(nc, null, ns.name, makePropertyList(ns.props, log));
+			String nodeId = ns.label.trim() + ":" + ns.name.trim();
 			if (nodes.containsKey(nodeId))
-				log.severe("duplicate node found ("+nodeId+") - ignoring the second one");
+				log.severe("duplicate node found (" + nodeId + ") - ignoring the second one");
 			else
-				nodes.put(nodeId,n);
-			if (ns.parent!=null) {
-				String parentId = ns.parent.label.trim()+":"+ns.parent.name.trim();
+				nodes.put(nodeId, n);
+			if (ns.parent != null) {
+				String parentId = ns.parent.label.trim() + ":" + ns.parent.name.trim();
 				TreeNode parent = nodes.get(parentId); // parent has always been treated before
 				n.setParent(parent);
 				parent.addChild(n);
@@ -250,13 +262,13 @@ public class TreeParser extends MinimalGraphParser {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * 
 	 * @return the tree built from this parser
 	 */
 	public Tree<? extends TreeNode> graph() {
-		if (tree==null)
+		if (tree == null)
 			buildTree();
 		return tree;
 	}
@@ -268,18 +280,18 @@ public class TreeParser extends MinimalGraphParser {
 		sb.append("Tree specification\n");
 		if (!treeProps.isEmpty())
 			sb.append("Graph properties:\n");
-		for (propSpec p:treeProps)
+		for (propSpec p : treeProps)
 			sb.append('\t').append(p.toString()).append('\n');
-		for (treeNodeSpec n:nodeSpecs) {
+		for (treeNodeSpec n : nodeSpecs) {
 			sb.append(n.toString()).append('\n');
-			for (propSpec p:n.props)
+			for (propSpec p : n.props)
 				sb.append("\t").append(p.toString()).append('\n');
-			if (n.parent==null)
+			if (n.parent == null)
 				sb.append("\tROOT NODE\n");
 			else
 				sb.append("\tparent ").append(n.parent.toString()).append('\n');
 		}
 		return sb.toString();
 	}
-	
+
 }
