b95164dfac0ca82caa132962dd2cf0bcc641a846
Jacques Gignoux <jacques.gignoux@upmc.fr>
2019-11-06 10:38:40 +0100

    testing parsing of edge properties (OK)

--- omugi/test/fr/cnrs/iees/io/parsing/impl/GraphParserTest.java
+++ omugi/test/fr/cnrs/iees/io/parsing/impl/GraphParserTest.java
@@ -102,6 +102,34 @@ class GraphParserTest {
 			"pnode name5\n",
 			"  prop1 = Integer(0)"};
 	
+	// testing for Edge properties
+	String[] edgePropTest = {"graph // checking the new ids\n", 
+			"  scope=String(\"edgePropTest\")",
+			"  node=String(\"fr.cnrs.iees.graph.impl.ALNode\")\n",
+			"  pnode=String(\"fr.cnrs.iees.graph.impl.ALDataNode\")\n",
+			"  edge=String(\"fr.cnrs.iees.graph.impl.ALEdge\")\n",
+			"  pedge=String(\"fr.cnrs.iees.graph.impl.ALDataEdge\")\n", 
+			"  mutable=Boolean(true)\n",
+			"\n",
+			"// nodes\n", 
+			"pnode\n",
+			"  prop1=Integer(1)\n", 
+			"  prop2 =Double(2.0)\n", 
+			"  prop3= String(\"blabla\")\n", 
+			"  prop4 = Boolean(true)\n",
+			"node name2\n",
+			"node name3\n", 
+			"\n",
+			"// edges\n", 
+			"[pnode:] edge name1 [node:name2]\n", 
+			"[pnode:] pedge name2 [node:name2 ]\n",
+			"  propE=Integer(17)\n",
+			"[node:name2 ] edge [node:name3]\n", 
+			"// more nodes\n",
+			"pnode name5\n",
+			"  prop1 = Integer(0)"};
+
+	
 	@Test
 	void testParse() {
 		GraphParser gp = new GraphParser(new GraphTokenizer(test));
@@ -173,5 +201,44 @@ class GraphParserTest {
 		assertEquals(g.nNodes(),4); // 4 nodes
 		assertEquals(g.getClass().getSimpleName(),"ALGraph");
 	}		
-	
+
+	@Test
+	void testParse3() {
+		GraphParser gp = new GraphParser(new GraphTokenizer(edgePropTest));
+		gp.parse();
+//		System.out.println(gp.toString());	
+		assertEquals(gp.toString(),"Graph specification\n" + 
+				"Graph properties:\n" + 
+				"	scope:String=edgePropTest\n" + 
+				"	node:String=fr.cnrs.iees.graph.impl.ALNode\n" + 
+				"	pnode:String=fr.cnrs.iees.graph.impl.ALDataNode\n" + 
+				"	edge:String=fr.cnrs.iees.graph.impl.ALEdge\n" + 
+				"	pedge:String=fr.cnrs.iees.graph.impl.ALDataEdge\n" + 
+				"	mutable:Boolean=true\n" + 
+				"Nodes:\n" + 
+				"	pnode:\n" + 
+				"		prop1:Integer=1\n" + 
+				"		prop2:Double=2.0\n" + 
+				"		prop3:String=blabla\n" + 
+				"		prop4:Boolean=true\n" + 
+				"	node:name2\n" + 
+				"	node:name3\n" + 
+				"	pnode:name5\n" + 
+				"		prop1:Integer=0\n" + 
+				"Edges:\n" + 
+				"	edge:name1 [pnode:-->node:name2]\n" + 
+				"	pedge:name2 [pnode:-->node:name2]\n" + 
+				"		propE:Integer=17\n" + 
+				"	edge: [node:name2-->node:name3]\n");
+	}
+
+	@Test
+	void testGraph3() {
+		GraphParser gp = new GraphParser(new GraphTokenizer(edgePropTest));
+		Graph<?,?> g = gp.graph();
+		System.out.println(g.toString());
+		assertEquals(g.nNodes(),4); // 4 nodes
+		assertEquals(g.getClass().getSimpleName(),"ALGraph");
+	}		
+
 }
