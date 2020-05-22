8766d87fe3beaca2835ffa3add66904e0f30a014
Ian Davies <ian.davies@anu.edu.au>
2020-05-22 11:51:59 +1000

    bug fix in StringTable.valueOf

--- omugi/src/au/edu/anu/rscs/aot/collections/tables/StringTable.java
+++ omugi/src/au/edu/anu/rscs/aot/collections/tables/StringTable.java
@@ -76,10 +76,12 @@ public class StringTable extends TableAdapter {
 //	}
 
 	public static StringTable valueOf(String value, char[][] bdel, char[] isep) {
+		value = value.trim();
 		if ((value == null) || value.isBlank() || value.isEmpty())
 			return null;
 		String ss = TableAdapter.getBlockContent(value, bdel[TABLEix]);
 		String d = ss.substring(0, ss.indexOf(bdel[DIMix][BLOCK_CLOSE]) + 1);
+		d = d.trim();
 		StringTable result = new StringTable(readDimensioners(d, bdel[DIMix], isep[DIMix]));
 		ss = ss.substring(ss.indexOf(bdel[DIMix][BLOCK_CLOSE]) + 1);
 		StringBuilder sb = new StringBuilder();
