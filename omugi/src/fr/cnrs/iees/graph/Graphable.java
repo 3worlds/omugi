0303795811178cca8648eef3eb05d28f9d4cc7cc
Jacques Gignoux <jacques.gignoux@upmc.fr>
2021-01-13 15:58:20 +0100

    adding Graphable interface

---
+++ omugi/src/fr/cnrs/iees/graph/Graphable.java
@@ -0,0 +1,43 @@
+/**************************************************************************
+ *  OMUGI - One More Ultimate Graph Implementation                        *
+ *                                                                        *
+ *  Copyright 2018: Shayne Flint, Jacques Gignoux & Ian D. Davies         *
+ *       shayne.flint@anu.edu.au                                          * 
+ *       jacques.gignoux@upmc.fr                                          *
+ *       ian.davies@anu.edu.au                                            * 
+ *                                                                        *
+ *  OMUGI is an API to implement graphs, as described by graph theory,    *
+ *  but also as more commonly used in computing - e.g. dynamic graphs.    *
+ *  It interfaces with JGraphT, an API for mathematical graphs, and       *
+ *  GraphStream, an API for visual graphs.                                *
+ *                                                                        *
+ **************************************************************************                                       
+ *  This file is part of OMUGI (One More Ultimate Graph Implementation).  *
+ *                                                                        *
+ *  OMUGI is free software: you can redistribute it and/or modify         *
+ *  it under the terms of the GNU General Public License as published by  *
+ *  the Free Software Foundation, either version 3 of the License, or     *
+ *  (at your option) any later version.                                   *
+ *                                                                        *
+ *  OMUGI is distributed in the hope that it will be useful,              *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *  GNU General Public License for more details.                          *                         
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public License     *
+ *  along with OMUGI.  If not, see <https://www.gnu.org/licenses/gpl.html>*
+ *                                                                        *
+ **************************************************************************/
+package fr.cnrs.iees.graph;
+
+/**
+ * An interface for objects that can be transformed into a graph
+ * 
+ * @author Jacques Gignoux - 13 janv. 2021
+ *
+ */
+public interface Graphable<N extends Node, E extends Edge> {
+	
+	public Graph<N,E> asGraph();
+
+}
